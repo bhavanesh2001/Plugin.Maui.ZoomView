d8acccb8da96eeb60a216cb966307e6373fc78e4
Bhavanesh N <bhavanesh001@gmail.com>
2025-07-06 22:20:21 +0530

    [housekeeping] cleanup

--- src/Plugin.Maui.ZoomView/Platforms/iOS/ZoomViewHandler.cs
+++ src/Plugin.Maui.ZoomView/Platforms/iOS/ZoomViewHandler.cs
@@ -1,6 +1,5 @@
 using Microsoft.Maui.Platform;
 using Plugin.Maui.ZoomView.Platforms.iOS;
-using UIKit;
 
 namespace Plugin.Maui.ZoomView;
 
@@ -8,7 +7,7 @@ public partial class ZoomViewHandler
 {
     protected override PlatformZoomView CreatePlatformView()
     {
-       return new PlatformZoomView();
+        return new PlatformZoomView();
     }
 
     protected override void DisconnectHandler(PlatformZoomView platformView)
@@ -17,32 +16,32 @@ public partial class ZoomViewHandler
         base.DisconnectHandler(platformView);
     }
 
-    
+
     public static void MapContent(ZoomViewHandler handler, IZoomView view)
     {
-        if(handler.MauiContext is null) throw new InvalidOperationException("MauiContext can not be null");
-        
-        if (handler.PlatformView is not null && view.Content is not null)
+        if (handler.MauiContext is null) throw new InvalidOperationException("MauiContext can not be null");
+
+        if (handler.IsConnected() && view.Content is not null)
         {
             var content = view.Content.ToPlatform(handler.MauiContext);
             handler.PlatformView.AddSubview(content);
         }
-        
+
     }
 
     public static void MapZoomOnDoubleTap(ZoomViewHandler handler, IZoomView view)
     {
-       if(handler is not null && handler.PlatformView is not null)
-       {    
-            handler.PlatformView.SetupDoubleTapGesture(view.ZoomInOnDoubleTap,view.ZoomOutOnDoubleTap);
-       }
+        if (handler.IsConnected())
+        {
+            handler.PlatformView.SetupDoubleTapGesture(view.ZoomInOnDoubleTap, view.ZoomOutOnDoubleTap);
+        }
     }
 
-        public static void MapReset(ZoomViewHandler handler, IZoomView view,object? args)
+    public static void MapReset(ZoomViewHandler handler, IZoomView view, object? args)
+    {
+        if (handler.IsConnected())
         {
-            if (handler?.PlatformView == null)
-                return;
-                
-            handler.PlatformView?.ResetZoom();
+            handler.PlatformView.ResetZoom();
         }
+    }
 }
